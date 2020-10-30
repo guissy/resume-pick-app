905f5fff4a997a6080646859d2d88146d27e3df8
guissy <guissy@qq.com>
2020-10-30 14:30:06 +0800

    feat(resume): support sort by score or workAge

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # resume pick app
 
 ## base electron
+
+## fix
+
+node_modules/textract/lib/extract.js:82
+
+```js
+extractors = fs.readdirSync(extractorPath).map(function (item) {
+  var fullExtractorPath = path.join(extractorPath, item);
+  // get the extractor
+  // eslint-disable-next-line global-require
+  return require(fullExtractorPath);
+});
+```
+
+replace with
+
+```js
+extractors = [
+  require('./extractors/doc'),
+  require('./extractors/docx'),
+  require('./extractors/doc-osx'),
+  require('./extractors/text'),
+];
+```
--- app/features/ScoreList.tsx
+++ app/features/ScoreList.tsx
@@ -90,6 +90,37 @@ const ScoreList: React.FC<Props> = ({ onClickResume, onClickTable }) => {
         });
     }
   }, [config, scores]);
+  const [sort, setSort] = React.useState<
+    '' | 'workAgeUp' | 'workAgeDown' | 'scoreUp' | 'scoreDown'
+  >('');
+  const onClickWorkAge = React.useCallback(() => {
+    setSort((wa) => {
+      if (wa !== 'workAgeUp' && wa !== 'workAgeDown') {
+        return 'workAgeDown';
+      }
+      if (wa === 'workAgeUp') {
+        return '';
+      }
+      if (wa === 'workAgeDown') {
+        return 'workAgeUp';
+      }
+      return '';
+    });
+  }, []);
+  const onClickScore = React.useCallback(() => {
+    setSort((wa) => {
+      if (wa !== 'scoreUp' && wa !== 'scoreDown') {
+        return 'scoreDown';
+      }
+      if (wa === 'scoreUp') {
+        return '';
+      }
+      if (wa === 'scoreDown') {
+        return 'scoreUp';
+      }
+      return '';
+    });
+  }, []);
   return scores.length > 0 ? (
     <div
       role="presentation"
@@ -103,49 +134,104 @@ const ScoreList: React.FC<Props> = ({ onClickResume, onClickTable }) => {
               序号
             </td>
             <td className={styles.td}>文件</td>
-            <td className={styles.td} style={{ width: 50 }}>
-              经验
+            <td className={`${styles.td} ${styles.sort}`} style={{ width: 50 }}>
+              <button
+                className={styles.sortBtn}
+                type="button"
+                onClick={onClickWorkAge}
+              >
+                <span>经验</span>
+                <div className={styles.caretInit}>
+                  <i
+                    className={`fa fa-caret-up ${
+                      sort === 'workAgeUp' ? styles.active : ''
+                    }`}
+                  />
+                  <i
+                    className={`fa fa-caret-down ${
+                      sort === 'workAgeDown' ? styles.active : ''
+                    }`}
+                  />
+                </div>
+              </button>
             </td>
-            <td className={styles.td}>分数</td>
+            <td className={`${styles.td} ${styles.sort}`}>
+              <button
+                className={styles.sortBtn}
+                type="button"
+                onClick={onClickScore}
+              >
+                <span>分数</span>
+                <div className={styles.caretInit}>
+                  <i
+                    className={`fa fa-caret-up ${
+                      sort === 'scoreUp' ? styles.active : ''
+                    }`}
+                  />
+                  <i
+                    className={`fa fa-caret-down ${
+                      sort === 'scoreDown' ? styles.active : ''
+                    }`}
+                  />
+                </div>
+              </button>
+            </td>
             <td colSpan={3} className={styles.td} style={{ width: '60%' }}>
               关键字
             </td>
           </tr>
         </thead>
         <tbody>
-          {scores.map((v, i) => (
-            <tr key={v.name}>
-              <td className={styles.td}>{i + 1}</td>
-              <td className={styles.td}>
-                <button
-                  type="button"
-                  className={styles.link}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    onClickResume(v.text);
-                  }}
-                >
-                  {v.name}
-                  <br />
-                  {trackPhone(v.text)}
-                </button>
-              </td>
-              <td className={styles.td}>{trackWorkAge(v.text)}</td>
-              <td className={styles.score}>{v.score?.toFixed(2)}</td>
-              {v.keywords.map((k) => (
-                <td className={styles.td} key={k.name}>
-                  {k.children.map((w) => (
-                    <span key={w.name} className={styles.scoreItem}>
-                      <span className={styles.subName}>{w.name}</span>
-                      <span className={styles.gained}>
-                        {w.gained.toFixed(1)}
-                      </span>
-                    </span>
-                  ))}
+          {scores
+            .map((s) => ({ ...s, age: parseInt(s.workAge, 10) || 0 }))
+            .sort((a, b) => {
+              if (sort === 'workAgeDown') {
+                return a.workAge > b.workAge ? -1 : 1;
+              }
+              if (sort === 'workAgeUp') {
+                return a.workAge < b.workAge ? -1 : 1;
+              }
+              if (sort === 'scoreDown') {
+                return a.score > b.score ? -1 : 1;
+              }
+              if (sort === 'scoreUp') {
+                return a.score < b.score ? -1 : 1;
+              }
+              return a.score > b.score ? -1 : 1;
+            })
+            .map((v, i) => (
+              <tr key={v.name}>
+                <td className={styles.td}>{i + 1}</td>
+                <td className={styles.td}>
+                  <button
+                    type="button"
+                    className={styles.link}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      onClickResume(v.text);
+                    }}
+                  >
+                    {v.name}
+                    <br />
+                    {trackPhone(v.text)}
+                  </button>
                 </td>
-              ))}
-            </tr>
-          ))}
+                <td className={styles.td}>{trackWorkAge(v.text)}</td>
+                <td className={styles.score}>{v.score?.toFixed(2)}</td>
+                {v.keywords.map((k) => (
+                  <td className={styles.td} key={k.name}>
+                    {k.children.map((w) => (
+                      <span key={w.name} className={styles.scoreItem}>
+                        <span className={styles.subName}>{w.name}</span>
+                        <span className={styles.gained}>
+                          {w.gained.toFixed(1)}
+                        </span>
+                      </span>
+                    ))}
+                  </td>
+                ))}
+              </tr>
+            ))}
         </tbody>
       </table>
       <footer className={styles.footer}>
